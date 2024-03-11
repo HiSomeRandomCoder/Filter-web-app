70896778ba57b8dd2343b5cfe99a4d55806d3d90
Someone <91122743+HiSomeRandomCoder@users.noreply.github.com>
2024-03-11 16:56:11 +1100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+function preload() {
+}
+
+function setup() {
+    let canvas = createCanvas(640, 480);
+    canvas.parent('canvas');
+}
+
+function draw() {
+}
+
+function saveFilteredImage() {
+}
+
+document.getElementById('saveButton').addEventListener('click', saveFilteredImage);
