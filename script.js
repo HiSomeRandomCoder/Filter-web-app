a6e4a330509a9729015d48a28f71019dc290120f
Someone <91122743+HiSomeRandomCoder@users.noreply.github.com>
2024-04-02 18:18:35 +1100

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+noseX=0;
+noseY=0;
 
 function preload() {
+    clown_nose = loadImage("https://i.postimg.cc/ZKkFH8r3/Clown-Nose.png");
 }
+
 
 function setup() {
     canvas = createCanvas(300, 300);
@@ -8,6 +12,7 @@ function setup() {
     video=createCapture(VIDEO);
     video.size(300,300);
     video.hide();
+
     poseNet=ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPoses);
 }
@@ -15,15 +20,24 @@ function setup() {
 function gotPoses(results){
     if(results.length>0){
         console.log(results);
-        console.log("nose x = "+results[0].pose.nose.x);
-        console.log("nose y = "+results[0].pose.nose.y);
+        noseX=results[0].pose.nose.x
+        noseY=results[0].pose.nose.y
+        console.log("nose x = "+noseX);
+        console.log("nose y = "+noseY);
         
+    }
+
+    function modelLoaded(){
+        console.log('PoseNet is Initialized');
     }
 }
 function draw() {
+    image(video, 0, 0, 300, 300);
+    image(clown_nose, noseX, noseY, 30, 30)
 }
 
-function saveFilteredImage() {
+function take_snapshot() {
+    save('myFilterImage.png')
 }
 
-document.getElementById('saveButton').addEventListener('click', saveFilteredImage);
+
